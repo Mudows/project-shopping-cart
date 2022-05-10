45d3d45475b16c0d315c2d6e594f32fdf4b8adc4
Diego Moraes Cezar <dmcezar@gmail.com>
2022-05-10 15:05:53 -0300

    Questão 2 - Finalizada.

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async ($ItemID) => {
+  const url = `https://api.mercadolibre.com/items/${$ItemID}`;
+  const itemRequisitado = await (await fetch(url)).json();
+  // console.log(itemRequisitado);
+  return itemRequisitado;
 };
+
+// fetchItem('MLB1341706310');
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // const { fetchProducts } = require('./helpers/fetchProducts');
 
+// const { fetchItem } = require("./helpers/fetchItem");
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -34,13 +36,37 @@ function createProductItemElement(sku, name, image) {
   // coloque seu código aqui
 } */
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement(sku, name, salePrice) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
+
+/* dadosProduto():
+ * recebe o id do produto, buscando as informações dele e cria um novo
+ * elemento no carrinho com suas informações.
+ */
+const dadosProduto = async (dados) => {
+  const id = dados.parentElement.firstChild.innerText;
+  const infoProd = await fetchItem(id);
+  const carrinho = document.querySelector('.cart__items');
+  const prodId = infoProd.id;
+  const prodNome = infoProd.title;
+  const prodValor = infoProd.price;
+  const item = createCartItemElement(prodId, prodNome, prodValor);
+  carrinho.appendChild(item);
+};
+
+/* adicionaProduto():
+ * Adiciona um evetListener aos botões, chamando a função dadosProduto que irá
+ * armazenar dados específicos do produto selecionado e exibilo no carrinho.
+ */
+const adicionaProduto = () => {
+  const botaoAdiciona = document.querySelectorAll('.item__add');
+  botaoAdiciona.forEach((botao) => botao.addEventListener('click', () => dadosProduto(botao)));
+};
 
 window.onload = async () => {
   const produtos = await fetchProducts('computador');
@@ -49,4 +75,5 @@ window.onload = async () => {
     document.querySelector('.items')
       .appendChild(createProductItemElement(element.id, element.title, element.thumbnail));
   });
+  adicionaProduto();
 };

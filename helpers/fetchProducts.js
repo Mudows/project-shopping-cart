d818693e671d157e53af0971eef28b105ab82284
Diego Moraes Cezar <dmcezar@gmail.com>
2022-05-10 10:32:02 -0300

    Começando a questão 1.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async ($QUERY) => {
+  if (!$QUERY) throw new Error('You must provide an url');
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
+  const produtosJason = await (await fetch(url)).json();
+  console.log(produtosJason);
+  return produtosJason;
 };
+
+// fetchProducts('computador');
+// console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {

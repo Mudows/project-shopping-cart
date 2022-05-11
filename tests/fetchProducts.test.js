a169d23e36b38dcef5634f3b45cf5bb1837ce2fc
Diego Moraes Cezar <dmcezar@gmail.com>
2022-05-11 19:25:13 -0300

    Questão 8 - problema com o window consertado. Resultado de alguns testes ainda não batem.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,7 +3,7 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  it('Se fetchProducts é uma função; ', async () => {
+  it('Se fetchProducts é uma função; ', () => {
     expect(typeof fetchProducts).toBe('function');
   })
   it('Se fetch foi chamada ao executar fetchProducts com o argumento "computador"; ', async () => {
@@ -15,11 +15,9 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   })
   it('O retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo; ', async () => {
-    const retorno = await fetchProducts('computador');
-    expect(retorno).toBeEqualTo(computadorSearch);
+    await expect(await fetchProducts('computador')).toEqual(computadorSearch);
   })
   it('Ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url; ', async () => {
-    const retorno = await fetchProducts();
-    expect(retorno).toBeEqualTo(new Error('You must provide an url'));
+    await expect(fetchProducts()).rejects.toThrowError('You must provide an url');
   })
 });
